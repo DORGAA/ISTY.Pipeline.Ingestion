3af17734b8f9c8f9a859f81a4ca625686083bdbd
DORGAA <said1dorgaa@gmail.com>
2021-01-17 17:42:47 +0100

    Reading json file from S3 Bucket

--- Ingestion-Pipeline/src/app.py
+++ Ingestion-Pipeline/src/app.py
@@ -1,7 +1,21 @@
 import json
-
+import boto3
 # import requests
 
 
+def read_file(bucket, file):
+    s3 = boto3.client('s3')
+    with open('/tmp/file.json', 'wb') as f:
+        s3.download_fileobj(bucket, file, f)
+
+    with open('/tmp/file.json') as json_file:
+        data = json.load(json_file)
+
+    print(data)
+
+
 def lambda_handler(event, context):
-    print('Hello said!')
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
+    read_file(bucket_name, file_key)
+
