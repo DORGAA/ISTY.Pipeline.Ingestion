adb7f13acbcf072491aaa5541c79907423da26d6
DORGAA <said1dorgaa@gmail.com>
2021-01-17 17:28:20 +0100

    debug: CI/CD

--- Ingestion-Pipeline/dep_script.sh
+++ Ingestion-Pipeline/dep_script.sh
@@ -1,11 +1,11 @@
-
+#!/bin/bash
 set -e
 
 
-NUMBER=$1
+NMBR=$1
 pushd "Ingestion-Pipeline"
 sam build
 
-sam deploy --no-fail-on-empty-changeset --parameter-overrides "ParameterKey=Number,ParameterValue=${NUMBER}"
+sam deploy --no-fail-on-empty-changeset --parameter-overrides "ParameterKey=Number,ParameterValue=${NMBR}"
 
 popd
