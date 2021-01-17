c99ef47953726255a27f8fd7066dab6d6afcdf6d
DORGAA <said1dorgaa@gmail.com>
2021-01-17 17:22:14 +0100

    debug: CI/CD

--- Ingestion-Pipeline/dep_script.sh
+++ Ingestion-Pipeline/dep_script.sh
@@ -1,6 +1,11 @@
 
-NUMBER=$1
+set -e
 
+
+NUMBER=$1
+pushd "Ingestion-Pipeline"
 sam build
 
 sam deploy --no-fail-on-empty-changeset --parameter-overrides "ParameterKey=Number,ParameterValue=${NUMBER}"
+
+popd
