0fb10af04d4f5ace25fd652d89f0b5b59ecd87a0
DORGAA <said1dorgaa@gmail.com>
2021-01-23 16:55:48 +0100

    Adding ingestion code

--- Ingestion-Pipeline/src/app.py
+++ Ingestion-Pipeline/src/app.py
@@ -1,6 +1,9 @@
 import json
 import boto3
+import os
 # import requests
+list_under_7 = []
+list_under_10 = []
 
 
 def read_file(bucket, file):
@@ -11,11 +14,83 @@ def read_file(bucket, file):
     with open('/tmp/file.json') as json_file:
         data = json.load(json_file)
 
-    print(data)
+    return data
+
+
+def average_and_count(data):
+    count = 0
+    total = 0
+    under_7 = 0
+    for key, value in data["notes"].items():
+        total += value
+        count += 1
+        if value < 7:
+            under_7 += 1
+            list_under_7.append(key)
+        elif 10 > value >= 7:
+            list_under_10.append(key)
+    avg = total / count
+    print(avg)
+    return avg, under_7
+
+
+def result(under_7, avg, data):
+    credits = []
+    ratt = []
+    if under_7 < 2:
+        if avg >= 10:
+            student_result = 'adm'
+        else:
+            student_result = 'rat'
+            ratt.extend(list_under_7)
+            ratt.extend(list_under_10)
+    elif under_7 == 2:
+        if avg >= 10:
+            student_result = 'admcr'
+            credits.extend(list_under_7)
+            credits.extend(list_under_10)
+        else:
+            student_result = 'rat'
+            ratt.extend(list_under_7)
+            ratt.extend(list_under_10)
+    elif under_7 == 3:
+        student_result = 'rat'
+        ratt.extend(list_under_7)
+        ratt.extend(list_under_10)
+    else:
+        student_result = 'rdb'
+        if data["resultat_Precedent"] == "rdb":
+            student_result = 'expl'
+    result_object = {
+        "matricule": data["matricule"],
+        "annee": data["annee"],
+        "nom": data["nom"],
+        "prenom": data["prenom"],
+        "resultat": student_result,
+        "moyenne": str(avg),
+        "releve": data["notes"],
+
+    }
+    if credits:
+        result_object["credits"] = credits
+    if ratt:
+        result_object["rattrapage"] = ratt
+    list_under_7.clear()
+    list_under_10.clear()
+    return result_object
+
+
+def dynamodb_put_object(object):
+    table = boto3.resource("dynamodb").Table(os.environ["TABLE_NAME"])
+    table.put_item(Item=object)
 
 
 def lambda_handler(event, context):
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
-    read_file(bucket_name, file_key)
+    data = read_file(bucket_name, file_key)
+    avg, count = average_and_count(data)
+    s_result = result(count, avg, data)
+    print(s_result)
+    dynamodb_put_object(s_result)
 
