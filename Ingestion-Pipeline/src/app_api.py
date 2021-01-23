2c7c7c52674d9445718956b97dab2c9781606c39
DORGAA <said1dorgaa@gmail.com>
2021-01-23 17:21:36 +0100

    fix: setup & test flask

--- Ingestion-Pipeline/src/app_api.py
+++ Ingestion-Pipeline/src/app_api.py
@@ -1,8 +1,15 @@
-from flask import Flask
+import boto3
+from flask import request, jsonify
+from flask_lambda import FlaskLambda
 
-app = Flask(__name__)
+app = FlaskLambda(__name__)
 
 
-@app.route("/hello")
-def hello():
-    return {"result": "Hello World!"}
+@app.route('/hello', methods=('GET',))
+def create_list():
+    return jsonify({"Result": "Hello world"}), 201
+
+
+@app.route('/hello', methods=('POST',))
+def create():
+    return jsonify({"Result": "world"}), 201
