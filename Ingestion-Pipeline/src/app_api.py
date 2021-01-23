be4348329f7349a04b8693bb363a74305e8195a7
DORGAA <32481242+DORGAA@users.noreply.github.com>
2021-01-23 19:10:32 +0100

    API: Get annee

--- Ingestion-Pipeline/src/app_api.py
+++ Ingestion-Pipeline/src/app_api.py
@@ -3,13 +3,26 @@ from flask import request, jsonify
 from flask_lambda import FlaskLambda
 
 app = FlaskLambda(__name__)
-
+student_table_name = 'isty-ingestion-table'
 
 @app.route('/hello', methods=('GET',))
 def create_list():
     return jsonify({"Result": "Hello world"}), 201
 
 
-@app.route('/hello', methods=('POST',))
-def create():
-    return jsonify({"Result": "world"}), 201
+@app.route('/annee', methods=('GET',))
+def annee():
+    table = boto3.resource("dynamodb").Table(student_table_name)
+    result = table.scan(AttributesToGet=['annee'])
+    items = []
+
+    items.extend(result["Items"])
+
+    while "LastEvaluatedKey" in result:
+        result = table.scan()
+        items.extend(result["Items"])
+    confs = []
+    for item in items:
+        confs.append(item["annee"])
+
+    return jsonify(confs), 201
