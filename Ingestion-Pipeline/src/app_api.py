c9cb2e72fd59ef243d3b2e63a3f955150886c313
DORGAA <32481242+DORGAA@users.noreply.github.com>
2021-01-24 17:09:40 +0100

    Update app_api.py

--- Ingestion-Pipeline/src/app_api.py
+++ Ingestion-Pipeline/src/app_api.py
@@ -23,7 +23,7 @@ student_table_name = os.environ["TABLE_NAME"]
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:4200')
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Api-Key')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
