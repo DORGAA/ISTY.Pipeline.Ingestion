aff6ef64a83697c7b79dd8041575afbfdb2d6391
DORGAA <said1dorgaa@gmail.com>
2021-01-24 13:20:17 +0100

    Disable CORS

--- Ingestion-Pipeline/src/app_api.py
+++ Ingestion-Pipeline/src/app_api.py
@@ -5,7 +5,6 @@ import decimal
 import os
 import flask.json
 from flask_lambda import FlaskLambda
-from flask_cors import CORS, cross_origin
 
 
 class MyJSONEncoder(flask.json.JSONEncoder):
@@ -15,21 +14,26 @@ class MyJSONEncoder(flask.json.JSONEncoder):
             return str(obj)
         return super(MyJSONEncoder, self).default(obj)
 
+
 app = FlaskLambda(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 app.json_encoder = MyJSONEncoder
 student_table_name = os.environ["TABLE_NAME"]
 
 
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Content-Type'] = 'application/json'
+    return response
+
+
 @app.route('/hello', methods=('GET',))
-@cross_origin()
 def hello_world():
     return jsonify({"Result": "Hello world"}), 200
 
 
 @app.route('/annee', methods=('GET',))
-@cross_origin()
 def annee():
     table = boto3.resource("dynamodb").Table(student_table_name)
     result = table.scan(AttributesToGet=['annee'])
@@ -42,14 +46,13 @@ def annee():
         items.extend(result["Items"])
     confs = []
     for item in items:
-    	if item["annee"] not in confs:
-        	confs.append(item["annee"])
+        if item["annee"] not in confs:
+            confs.append(item["annee"])
 
-    return jsonify(confs), 200
+    return jsonify(confs)
 
 
 @app.route('/matricules/<annee>', methods=('GET',))
-@cross_origin()
 def matricules(annee):
     table = boto3.resource("dynamodb").Table(student_table_name)
     results = table.query(KeyConditionExpression=Key('annee').eq(annee))
@@ -57,7 +60,6 @@ def matricules(annee):
 
 
 @app.route('/etudiant/<annee>/<matricule>', methods=('GET',))
-@cross_origin()
 def get_etudiant(annee, matricule):
     table = boto3.resource("dynamodb").Table(student_table_name)
     results = table.get_item(Key={"annee": annee, "matricule": matricule})
@@ -65,14 +67,13 @@ def get_etudiant(annee, matricule):
 
 
 @app.route('/etudiant/<annee>/<matricule>', methods=('DELETE',))
-@cross_origin()
 def delete_etudiant(annee, matricule):
     table = boto3.resource("dynamodb").Table(student_table_name)
     results = table.delete_item(Key={"annee": annee, "matricule": matricule})
     return jsonify(results["Item"]), 200
 
+
 @app.route('/etudiant/<annee>/<matricule>', methods=('POST',))
-@cross_origin()
 def post_etudiant(annee, matricule):
     table = boto3.resource("dynamodb").Table(student_table_name)
     table.put_item(request.json)
